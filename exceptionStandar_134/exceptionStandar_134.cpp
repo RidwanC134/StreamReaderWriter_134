ee6c7bdd144639f9094a6d92d2a4e6a0715aa56a
RidwanC134 <ridwanhh10@gmail.com>
2023-06-19 11:48:02 +0700

    Membuat include array agar bisa menggunakan obyek array

--- exceptionStandar_134/exceptionStandar_134.cpp
+++ exceptionStandar_134/exceptionStandar_134.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <exception> 
 // untuk obyek exception yang akan kita gunakan
-#include <array>
+#include <array> 
 // untuk obyek array yang akan kita gunakan
 using namespace std;
 
